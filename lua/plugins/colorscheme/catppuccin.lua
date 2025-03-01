8c734d4c858acc1b39ffff218ad6d58fa2fffd33
Wind He <g311571057@gmail.com>
2025-03-02 00:36:40 +0800

    colorscheme(catppuccin): move integration setup into plugin its own spec

--- lua/plugins/colorscheme/catppuccin.lua
+++ lua/plugins/colorscheme/catppuccin.lua
@@ -1,13 +1,8 @@
 ---@type LazyPluginSpec
 return {
   'catppuccin/nvim',
-  dir = '/home/heddxh/workspace/code/catppuccin.nvim',
+  -- dir = '/home/heddxh/workspace/code/catppuccin.nvim',
   name = 'catppuccin',
   priority = 1000,
-  opts = {
-    transparent_background = true,
-    integrations = {
-      aerial = true,
-    },
-  },
+  opts = { transparent_background = true },
 }
--- lua/plugins/ui/aerial.lua
+++ lua/plugins/ui/aerial.lua
@@ -1,32 +1,38 @@
----@type LazyPluginSpec
+---@type LazyPluginSpec[]
 return {
-  'stevearc/aerial.nvim',
-  event = 'VeryLazy',
-  dependencies = { 'onsails/lspkind.nvim' },
-  keys = {
-    { '<leader>o', '<Cmd>AerialToggle<CR>', desc = 'Open Aerial Outline' },
+  {
+    'stevearc/aerial.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'onsails/lspkind.nvim' },
+    keys = {
+      { '<leader>o', '<Cmd>AerialToggle<CR>', desc = 'Open Aerial Outline' },
+    },
+    opts = {
+      backends = { 'lsp', 'treesitter', 'markdown', 'asciidoc', 'man' },
+      layout = {
+        default_direction = 'float',
+        max_width = 100,
+      },
+      float = {
+        relative = 'editor',
+      },
+      close_automatic_events = { 'unfocus', 'switch_buffer', 'unsupported' },
+      nerd_font = 'lspkind-nvim',
+      filter_kind = {
+        'Class',
+        'Constructor',
+        'Enum',
+        'Function',
+        'Interface',
+        'Module',
+        'Method',
+        'Struct',
+        'Field',
+      },
+    },
   },
-  opts = {
-    backends = { 'lsp', 'treesitter', 'markdown', 'asciidoc', 'man' },
-    layout = {
-      default_direction = 'float',
-      max_width = 100,
-    },
-    float = {
-      relative = 'editor',
-    },
-    close_automatic_events = { 'unfocus', 'switch_buffer', 'unsupported' },
-    nerd_font = 'lspkind-nvim',
-    filter_kind = {
-      'Class',
-      'Constructor',
-      'Enum',
-      'Function',
-      'Interface',
-      'Module',
-      'Method',
-      'Struct',
-      'Field',
-    },
+  {
+    'catppuccin/nvim',
+    opts = { integrations = { aerial = true } },
   },
 }
